8248610412199aefb75cf5cca6ab4b3597e78ed6
Fernando Matzdorf <fernando@rindecuentas.org>
2020-06-11 11:14:23 -0500

    Added switch to enable output modes: db insertion or stdout streaming. Removed unnecessary logging.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let hrend = 0;
 
 const extractEntities = require('./lib/extract');
 const sendToDB = require('./lib/insert');
+const streamOut = require('./lib/stream');
 const monk = require('monk');
 const commandLineArgs = require('command-line-args');
 const _ = require('lodash');
@@ -11,14 +12,20 @@ const _ = require('lodash');
 const optionDefinitions = [
     { name: 'database', alias: 'd', type: String },
     { name: 'collection', alias: 'c', type: String },
-    { name: 'host', alias: 'h', type: String },
-    { name: 'port', alias: 'p', type: String },
+    { name: 'host', alias: 'h', type: String, defaultValue: 'localhost' },
+    { name: 'port', alias: 'p', type: String, defaultValue: '27017' },
+    { name: 'output', alias: 'o', type: String, defaultValue: 'stream' },
     { name: 'test', alias: 't', type: Boolean }
 ];
 const args = commandLineArgs(optionDefinitions);
 
 if(!args.database || !args.collection) {
     console.log('ERROR: no database or collection specified.');
+    process.exit(1);
+}
+if(args.output != 'db' && args.output != 'stream') {
+    console.log('ERROR: unsupported output value ' + args.output);
+    console.log('Supported values are: \n* db\n* stream');
     process.exit(1);
 }
 
@@ -33,7 +40,7 @@ else {
 }
 
 // Connect to MongoDB
-const url = 'mongodb://' + (args.host ? args.host : 'localhost') + ':' + (args.port ? args.port : '27017') + '/' + args.database;
+const url = 'mongodb://' + args.host + ':' + args.port + '/' + args.database;
 const db = monk(url);
 
 let entities = {
@@ -45,54 +52,60 @@ let entities = {
 }
 
 db.then( (db) => {
-    console.log('Connected to ' + args.database + '...');
+    if(args.output == 'db') console.log('Connected to ' + args.database + '...');
     const records = db.get(args.collection);
     let processed = 0;
     records.find(query)
         .each( (record, {close, pause, resume}) => {
-            const localRecord = _.clone(record);
-            delete record;
-
-            let c_release = localRecord.compiledRelease;
-            let releases = localRecord.releases;
+            let c_release = record.compiledRelease;
+            let releases = record.releases;
             processed++;
-            console.log(processed, c_release.ocid)
+
+            if(args.output == 'db') console.log(processed, c_release.ocid)
             extractEntities(c_release, releases, entities);
-            release = null;
+
+            // Cleanup...
+            delete releases;
+            delete c_release;
+            delete record;
         } )
         .then( () => {
             if(args.test) {
-                console.log(JSON.stringify(entities, null, 4));
-                process.exit();
+                streamOut(entities);
             }
-            console.log('Extraction complete! Sending to DB...');
-            sendToDB(entities, db)
-            .then( ( results ) => {
-                db.close();
-                hrend = process.hrtime(hrstart);
-                console.log('-------------------------------');
-                console.log('Persons found: ' + Object.keys(entities.persons).length);
-                console.log('Inserted ' + results[0].nInserted + ' persons.')
-                console.log('-------------------------------');
-                console.log('Companies found: ' + Object.keys(entities.companies).length);
-                console.log('Inserted ' + results[1].nInserted + ' companies.')
-                console.log('-------------------------------');
-                console.log('Institutions found: ' + Object.keys(entities.institutions).length);
-                console.log('Inserted ' + results[2].nInserted + ' institutions.')
-                console.log('-------------------------------');
-                console.log('States/Municipalities found: ' + Object.keys(entities.states).length);
-                console.log('Inserted ' + results[3].nInserted + ' states/municipalities.')
-                console.log('-------------------------------');
-                console.log('Memberships found: ' + Object.keys(entities.memberships).length);
-                console.log('Inserted ' + results[4].nInserted + ' memberships.')
-                console.log('-------------------------------');
-                console.log('Processed records: ' + processed);
-                console.log('Duration: ' + hrend[0] + '.' + hrend[1] + 's');
+            else if(args.output == 'db') {
+                console.log('Extraction complete! Sending to DB...');
+                sendToDB(entities, db)
+                .then( ( results ) => {
+                    db.close();
+                    hrend = process.hrtime(hrstart);
+                    console.log('-------------------------------');
+                    console.log('Persons found: ' + Object.keys(entities.persons).length);
+                    console.log('Inserted ' + results[0].nInserted + ' persons.')
+                    console.log('-------------------------------');
+                    console.log('Companies found: ' + Object.keys(entities.companies).length);
+                    console.log('Inserted ' + results[1].nInserted + ' companies.')
+                    console.log('-------------------------------');
+                    console.log('Institutions found: ' + Object.keys(entities.institutions).length);
+                    console.log('Inserted ' + results[2].nInserted + ' institutions.')
+                    console.log('-------------------------------');
+                    console.log('States/Municipalities found: ' + Object.keys(entities.states).length);
+                    console.log('Inserted ' + results[3].nInserted + ' states/municipalities.')
+                    console.log('-------------------------------');
+                    console.log('Memberships found: ' + Object.keys(entities.memberships).length);
+                    console.log('Inserted ' + results[4].nInserted + ' memberships.')
+                    console.log('-------------------------------');
+                    console.log('Processed records: ' + processed);
+                    console.log('Duration: ' + hrend[0] + '.' + hrend[1] + 's');
 
-                // Cleanup...
-                entities = null;
-                process.exit();
-            } );
+                    // Cleanup...
+                    entities = null;
+                    process.exit();
+                } );
+            }
+            else if(args.output == 'stream') {
+                streamOut(entities);
+            }
         } )
 } )
 .catch( (err) => { console.log(err); } );
